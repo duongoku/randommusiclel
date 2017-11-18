5ac764e21f7efb658b3a0db95e1e59d7c909b1f3
duongoku <nguyenvubinhduong@gmail.com>
2017-11-18 16:15:40 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const fs = require("fs");
+
+var app = express();
+var template = fs.readFileSync("home.html", "utf-8");
+var list = fs.readdirSync("./music");
+
+app.use("/", express.static(__dirname));
+
+function replace(str, tag, value) {
+	return str.replace(`[${tag}]`, value);
+}
+
+app.get("/", (req,res) => {
+	
+	var response = template;
+
+	var rand = Math.random()*(list.length-1);
+
+	var argv = list[Math.round(rand)].split(".");
+
+	response = replace(response,"songdetails",argv[0]);
+	response = replace(response,"song",list[Math.round(rand)]);
+
+	res.send(response);
+
+});
+
+app.listen(8888, () => {
+	console.log("App is listening on port 8888 . . .");
+});
